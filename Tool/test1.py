780440789b545c7ee79d5a6ef05e4cb80348d15c
minjoon-koo <115975651+minjoon-koo@users.noreply.github.com>
2022-11-08 01:50:09 -0800

    Delete Tool directory

--- Tool/test1.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-def git_clone():
-    f = open('git_clone.py','wb')
-    
-    
--- Tool/test2.py
+++
@@ -1,4 +0,0 @@
-from test1 import git_clone
-
-git_clone = git_clone()
-print(git_clone.mai)
